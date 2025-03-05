b6147232d71b90945044b24278b5a7d08b95900f
Jackyandsky <114648314+Jackyandsky@users.noreply.github.com>
2025-03-05 13:16:55 -0800

    Add src/App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// src/App.jsx
+import React from 'react';
+import WeeklySchedule from './components/WeeklySchedule';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <WeeklySchedule />
+    </div>
+  );
+}
+
+export default App;
